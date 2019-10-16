8601bf367a996a221c29c00caa49f293cf7cafd5
bulletTrain54 <mvishnurana@gmail.com>
2019-10-16 11:11:49 +0800

    JS adding static items to list

--- app.js
+++ app.js
@@ -1,53 +1,81 @@
-// STORAGE controller 
+// Storage Controller
 
 
-
-// ITEM controller 
+// Item Controller
 const ItemCtrl = (function(){
-    // Item constructor
+    // Item Constructor
     const Item = function(id, name, calories){
-        this.id = id; 
-        this.name = name; 
-        this.calories = calories; 
-
-        // Data Structure / State
-        const data = {
-            items: [
-                {id: 0, name: 'steak Dinner', calories: 1200}, 
-                {id: 1, name: 'Cookie', calories: 135}, 
-                {id: 2, name: 'Eggs', calories: 10}, 
-            ], 
-            currentItem: null, 
-            totalCalories: 0
-        }
-
-        // Public Methods
-        return {
-            getItems: function(){
-                return data.items;                 
-            }, 
-
-            logData: function(){
-                return data; 
-            }
-        }
+      this.id = id;
+      this.name = name;
+      this.calories = calories;
     }
-})(); 
-
-
-// UI controller 
-const UICtrl = (function(){
-
-})(); 
-
-// App controller 
-const App = (function(ItemCtrl, UICtrl){
-
-    // Public Method
-    return{
-        AppInit: function(){
-            console.log('App init!'); 
-        }
+  
+    // Data Structure / State
+    const data = {
+      items: [
+        {id: 0, name: 'Steak Dinner', calories: 1200},
+        {id: 1, name: 'Cookie', calories: 400},
+        {id: 2, name: 'Eggs', calories: 300}
+      ],
+      currentItem: null,
+      totalCalories: 0
     }
-
-})(ItemCtrl, UICtrl); 
+  
+    // Public methods
+    return {
+      getItems: function(){
+        return data.items;
+      },
+      logData: function(){
+        return data;
+      }
+    }
+  })();
+  
+  
+  
+  // UI Controller
+  const UICtrl = (function(){
+    const UISelectors = {
+      itemList: '#item-list'
+    }
+    
+    // Public methods
+    return {
+      populateItemList: function(items){
+        let html = '';
+  
+        items.forEach(function(item){
+          html += `<li class="collection-item" id="item-${item.id}">
+          <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content">
+            <i class="edit-item fa fa-pencil"></i>
+          </a>
+        </li>`;
+        });
+  
+        // Insert list items
+        document.querySelector(UISelectors.itemList).innerHTML = html;
+      }
+    }
+  })();
+  
+  
+  
+  // App Controller
+  const App = (function(ItemCtrl, UICtrl){
+  
+    // Public methods
+    return {
+      init: function(){
+        // Fetch items from data structure
+        const items = ItemCtrl.getItems();
+  
+        // Populate list with items
+        UICtrl.populateItemList(items);
+      }
+    }
+  })(ItemCtrl, UICtrl);
+  
+  // Initialize App
+  App.init();
